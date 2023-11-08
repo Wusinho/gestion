25ee8e671a7db04a9b86fca6fbce5bd5636e5da7
Heber Lazo <heberlazo@gmail.com>
2023-11-07 22:34:44 -0500

    Corrected invitation sent

--- app/controllers/project_participants_controller.rb
+++ app/controllers/project_participants_controller.rb
@@ -1,10 +1,12 @@
 class ProjectParticipantsController < ApplicationController
+
   def index
   end
 
   def create
     @project_participant = ProjectParticipant.new(project_participant_params)
     if @project_participant.save
+      render turbo_stream: turbo_stream.remove("user_#{@project_participant.user.id}")
     else
       turbo_error_message(@project_participant)
     end
@@ -16,6 +18,6 @@ class ProjectParticipantsController < ApplicationController
   private
 
   def project_participant_params
-    params.permit(:user_id, :project_id).merge(role: params[:role].to_i)
+    params.require(:project_participant).permit(:user_id, :project_id).merge(role: params[:role].to_i)
   end
 end
