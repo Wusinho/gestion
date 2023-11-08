f667c96a22fd3a65e8b2a08bb26d41fe2d01d4f9
Heber Lazo <heberlazo@gmail.com>
2023-11-07 20:39:57 -0500

    Added user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :name, :username, :email
-  validates_uniqueness_of :email
+  validates_presence_of :name, :username
+  validates_uniqueness_of :username
+  validates :email, presence: true, uniqueness: true, format: { with: /\A[^@\s]+@[^@\s]+\z/ }
 end
